af739e81c6cd25f815de637ba2de0fe43d8df64a
Juan Robledo <juannrob@gmail.com>
2023-02-27 10:25:09 -0300

    task 2 fixes

--- scripts/upcoming_events.js
+++ scripts/upcoming_events.js
@@ -7,14 +7,18 @@ const eventsMap = upcomingEvents.map((evento) =>
         <div class="card border border-dark rounded-0 p-2" key=${evento._id}>
             <img src="${evento.image}" class="card-img-top rounded-0" alt="${evento.name}">
             <div class="card-body">
-                <h5 class="card-title text-center">${evento.name}</h5>
-                <p class="card-text text-center h-75 d-flex align-items-center justify-content-center">${evento.description}</p>
+                <h5 class="card-title text-center fs-5">${evento.name}</h5>
+                <p class="text-center">${evento.category}</p>
+                <ul class="card-text h-75 d-flex flex-column justify-content-center">
+                <li>Date: <b>${evento.date}</b></li>
+                <li>Location: <b>${evento.place}</b></li>
+                </ul>
             </div>
             <div class="card-footer row flex-column flex-md-row align-items-center gy-3 border-0 bg-transparent d-flex justify-content-around 
                         justify-content-sm-between justify-content-lg-around justify-content-xxl-between">
                 <p class="col-6 col-md-5 col-lg-4 col-xl-5 col-xxl-5 align-baseline text-center">Price $${evento.price}</p>
                 <a href="./details.html" class="col-4 col-sm-8 col-md-6 col-lg-5 col-xl-4 col-xxl-6 
-                        see-more text-center text-reset px-0 px-sm-4 px-md-0 pb-1">ver más...</a>
+                        see-more text-center text-reset text-decoration-none px-0 px-sm-4 px-md-0 pb-1">ver más...</a>
             </div>
         </div>
     </div>`
