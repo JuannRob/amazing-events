4eeba00122456311e3b29ff2f914663e7d9c4471
Juan Robledo <juannrob@gmail.com>
2023-03-09 01:39:07 -0300

    task3 minor fixes

--- scripts/past_events.js
+++ scripts/past_events.js
@@ -11,24 +11,24 @@ const renderCards = (objArray) => {
     if (objArray.length > 0) {
         res = objArray.map((evento) =>
             `<div class="col-11 col-sm-5 col-xl-3 col-xxl-2">
-        <div class="card border border-dark rounded-0 p-2" key=${evento._id}>
-            <img src="${evento.image}" class="card-img-top rounded-0" alt="${evento.name}">
-            <div class="card-body">
-                <h5 class="card-title text-center fs-5">${evento.name}</h5>
-                <p class="text-center">${evento.category}</p>
-                <ul class="card-text h-75 d-flex flex-column justify-content-center">
-                <li>Date: <b>${evento.date}</b></li>
-                <li>Location: <b>${evento.place}</b></li>
-                </ul>
-            </div>
-            <div class="card-footer row flex-column flex-md-row align-items-center gy-3 border-0 bg-transparent d-flex justify-content-around 
-                        justify-content-sm-between justify-content-lg-around justify-content-xxl-between">
-                <p class="col-6 col-md-5 col-lg-4 col-xl-5 col-xxl-5 align-baseline text-center">Price $${evento.price}</p>
-                <a href="/details.html?evento=${evento._id}" class="col-4 col-sm-8 col-md-6 col-lg-5 col-xl-4 col-xxl-6 
-                        see-more text-center text-reset text-decoration-none px-0 px-sm-4 px-md-0 pb-1">Ver más...</a>
-            </div>
-        </div>
-    </div>`
+                <div class="card border border-dark rounded-0 p-2" key=${evento._id}>
+                    <img src="${evento.image}" class="card-img-top rounded-0" alt="${evento.name}">
+                    <div class="card-body">
+                        <h5 class="card-title text-center fs-5">${evento.name}</h5>
+                        <p class="text-center">${evento.category}</p>
+                        <ul class="card-text h-75 d-flex flex-column justify-content-center">
+                            <li>Date: <b>${evento.date}</b></li>
+                            <li>Location: <b>${evento.place}</b></li>
+                        </ul>
+                    </div>
+                    <div class="card-footer row flex-column flex-md-row align-items-center gy-3 border-0 bg-transparent d-flex justify-content-around 
+                            justify-content-sm-between justify-content-lg-around justify-content-xxl-between">
+                        <p class="col-6 col-md-5 col-lg-4 col-xl-5 col-xxl-5 align-baseline text-center">Price $${evento.price}</p>
+                        <a href="/details.html?evento=${evento._id}" class="col-4 col-sm-8 col-md-6 col-lg-5 col-xl-4 col-xxl-6 
+                            see-more text-center text-reset text-decoration-none px-0 px-sm-4 px-md-0 pb-1">Ver más...</a>
+                    </div>
+                </div>
+            </div>`
         ).join('');
     } else {
         res = '<div class="col-12 text-center py-5"><p class="fs-4">No se encontraron resultados.</p> <p class="fs-5">Prueba cambiando los filtros.</p></div>'
@@ -44,7 +44,7 @@ renderCards(pastEvents);
 //Guarda las categorias en un array omitiendo las repetidas.
 let catArr = [];
 events.forEach(evento => {
-    if (!catArr.includes(evento.category)) catArr.push(evento.category)
+    if (!catArr.includes(evento.category)) catArr.push(evento.category);
 });
 
 //Convierte las categorias en inputs de tipo checkbox.
@@ -80,7 +80,7 @@ const applyFilters = () => {
     }
 
     if (searchStr !== '') {
-        const res = filteredEvents.filter(evento => evento.name.trim().toLowerCase().includes(inputSearch.value.trim().toLowerCase()) || evento.description.trim().toLowerCase().includes(inputSearch.value.trim().toLowerCase()));
+        const res = filteredEvents.filter(evento => evento.name.trim().toLowerCase().includes(searchStr.trim().toLowerCase()) || evento.description.trim().toLowerCase().includes(searchStr.trim().toLowerCase()));
         filteredEvents = res;
     }
 
